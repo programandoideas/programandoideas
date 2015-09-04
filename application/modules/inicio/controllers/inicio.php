b56556072df5105c509247e62c6ec6003f6fb167
elias <elias@NB-CHINO>
2015-09-04 11:28:02 -0300

    Pruebas modulares

---
+++ application/modules/inicio/controllers/inicio.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Inicio extends MX_Controller {
+    public function index()
+    {
+        $this->load->view('index');
+    }
+}
