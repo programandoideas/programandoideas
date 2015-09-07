219b04defb75bdc42e2d5e162d95259da5706189
elias <elias@NB-CHINO>
2015-09-06 22:54:14 -0300

    Plantilla principal WWW.PROGRAMANDOIDEAS.CL

--- application/views/header.php
+++ application/views/header.php
@@ -16,10 +16,10 @@
     <script src="<?php echo base_url();?>assets/js/respond.min.js"></script>
     <![endif]-->       
     <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>assets/images/ico/logo.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/images/ico/alogo.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/images/ico/logo.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/images/ico/logo.png">
 </head><!--/head-->
 <body>
     <header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
