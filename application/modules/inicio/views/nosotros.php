5eb0b8c6856b4547c5226065a9d1ff3071b8731a
escipion27 <olavarriaph@hotmail.com>
2015-10-01 17:05:55 -0300

    Completando seccion quienes somos

--- application/modules/inicio/views/nosotros.php
+++ application/modules/inicio/views/nosotros.php
@@ -62,7 +62,7 @@
                 </div>
                 <div class="progress">
                     <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
-                        <span>PLSQL</span>
+                        <span>Oracle</span>
                     </div>
                 </div>
             </div>
