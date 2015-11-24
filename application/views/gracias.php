a522fb9b808999c377d128e10c80d24c4b36e54b
escipion <escipion@escipion-ThinkPad-X220>
2015-11-24 11:07:51 -0300

    Modificacion boton

--- application/views/gracias.php
+++ application/views/gracias.php
@@ -2,7 +2,7 @@
     <div class="col-md-offset-1 col-lg-10">
         <div class="alert alert-info" role="alert">
             <strong><span class="glyphicon glyphicon-thumbs-up"></span> </strong><?php echo $mensaje; ?>
-            &nbsp;&nbsp;<input type="button" class="btn btn-success btn-lg" value="Inicio">
+            &nbsp;&nbsp;<a  class="btn btn-success btn-lg" href="<?php echo base_url();?>">Inicio</a>
         </div>
     <!--** FIN MENSAJES **-->
     </div>
